c52fccb05c48a3c1c52645fe42c6526b66c4c655
iDevelopApps <administrator@Teacher-Server.local>
2014-12-04 15:32:39 -0500

    Initial Commit

---
+++ FirstProject/FirstProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FirstProject
+//
+//  Created by administrator on 12/4/14.
+//  Copyright (c) 2014 iDevelopApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FirstProject/FirstProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirstProject
+//
+//  Created by administrator on 12/4/14.
+//  Copyright (c) 2014 iDevelopApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
